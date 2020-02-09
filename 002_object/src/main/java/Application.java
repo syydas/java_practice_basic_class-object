f51e0b7b1ae21e1888fb821b73fa799f5d8ba13c
Asuu <suqinzheng_0624@163.com>
2020-02-09 17:22:50 +0800

    [Asu] feat: creat a student named lily and print all informations

--- 002_object/src/main/java/Application.java
+++ 002_object/src/main/java/Application.java
@@ -4,5 +4,11 @@ public class Application {
 
     public static void main(String[] args) {
 
+        Student lily = new Student("李莉", 20, "女");
+        System.out.println(lily.getName());
+        System.out.println(lily.getAge());
+        System.out.println(lily.getGender());
+        lily.study();
+        lily.rest();
     }
 }
